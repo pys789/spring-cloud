214c8c1672b2af051b9a833c8ef57f453c61a0cd
2998 <pengys@yunjiglobal.com>
2021-04-30 15:34:02 +0800

    提交

--- cloud-base/ribbon-consumer/src/main/java/com/cloud/ribbonconsumer/ConsumerController.java
+++ cloud-base/ribbon-consumer/src/main/java/com/cloud/ribbonconsumer/ConsumerController.java
@@ -1,12 +1,14 @@
 package com.cloud.ribbonconsumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 /**
  * @Description
@@ -34,6 +36,19 @@ public class ConsumerController {
         // ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
 
         return entity.getBody();
+    }
 
+    @RequestMapping("ribbon-consumer2")
+    @HystrixCommand(fallbackMethod = "helloFallback")
+    public String helloConsumer2() throws InterruptedException {
+        // Hystrix 默认超时时间为2000毫秒
+        Thread.sleep(new Random().nextInt(3000));
+        // 使用@LoadBalanced
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://hello-service/hello", String.class);
+        return entity.getBody();
+    }
+
+    public String helloFallback() {
+        return "error";
     }
 }
